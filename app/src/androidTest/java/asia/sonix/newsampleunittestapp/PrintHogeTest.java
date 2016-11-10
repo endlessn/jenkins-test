a8073a432efcd10b69d2610efe32386d0e0b34ac
steshima <steshima@sonix.asia>
2016-11-10 10:30:36 +0900

    add HogeTest2.java

---
+++ app/src/androidTest/java/asia/sonix/newsampleunittestapp/PrintHogeTest.java
@@ -0,0 +1,16 @@
+package asia.sonix.newsampleunittestapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by steshima on 2016/11/10.
+ */
+public class PrintHogeTest {
+
+    @Test
+    public void testPrintLowerCase() throws Exception {
+
+    }
+}
